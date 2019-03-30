98638a7eaf2d2ee22703677bc094c8fc1556d49e
Nícolas Oliveira <cc19193@g.unicamp.br>
2019-03-30 14:19:32 -0300

    Implementação do Exercício 3(Raíz cúbica)

--- TPProjetoI/Matematica.cs
+++ TPProjetoI/Matematica.cs
@@ -3,115 +3,126 @@ using System.Collections.Generic; // Necessário para uso de listas
 
 class Matematica
 {
-	static int nInt;
+    static int nInt;
 
-	public Matematica(int nInformado)
-	{
-		nInt = nInformado;
-	}
+    public Matematica(int nInformado)
+    {
+        nInt = nInformado;
+    }
 
-	public int Fatorial()
-	{
-		var fat = new Produtorio();
-		var contador = new Contador(1, nInt, 1);
-		while (contador.Prosseguir())
-			fat.Multiplicar(contador.Valor);
+    public int Fatorial()
+    {
+        var fat = new Produtorio();
+        var contador = new Contador(1, nInt, 1);
+        while (contador.Prosseguir())
+            fat.Multiplicar(contador.Valor);
 
-		return (int)fat.Valor;
-	}
+        return (int)fat.Valor;
+    }
 
-	public static string Divisores()
-	{
-		string lista = "";
-		var possivelDivisor = new Contador(2, nInt / 2, 1);
-		while (possivelDivisor.Prosseguir())
-		{
-			int resto = nInt % possivelDivisor.Valor;
-			if (resto == 0)
-				lista = lista + possivelDivisor.Valor + ", ";
-		}
-		lista = "1, " + lista + nInt;
-		return lista;
-	}
+    public static string Divisores()
+    {
+        string lista = "";
+        var possivelDivisor = new Contador(2, nInt / 2, 1);
+        while (possivelDivisor.Prosseguir())
+        {
+            int resto = nInt % possivelDivisor.Valor;
+            if (resto == 0)
+                lista = lista + possivelDivisor.Valor + ", ";
+        }
+        lista = "1, " + lista + nInt;
+        return lista;
+    }
 
-	public int MDC(int n2)
-	{
-		int dividendo = nInt;
-		int divisor = n2;
-		int resto;
-		do
-		{
-			resto = dividendo % divisor;
-			if (resto != 0)
-			{
-				dividendo = divisor;
-				divisor = resto;
-			}
-		}
-		while (resto != 0);
+    public int MDC(int n2)
+    {
+        int dividendo = nInt;
+        int divisor = n2;
+        int resto;
+        do
+        {
+            resto = dividendo % divisor;
+            if (resto != 0)
+            {
+                dividendo = divisor;
+                divisor = resto;
+            }
+        }
+        while (resto != 0);
 
-		return divisor;
-	}
+        return divisor;
+    }
 
-	public bool Palindromo()
-	{
-		int numero = nInt;
-		int aoContrario = 0;
-		while (numero > 0)
-		{
-			int quociente = numero / 10;
-			int resto = numero - 10 * quociente;
+    public bool Palindromo()
+    {
+        int numero = nInt;
+        int aoContrario = 0;
+        while (numero > 0)
+        {
+            int quociente = numero / 10;
+            int resto = numero - 10 * quociente;
 
-			aoContrario = aoContrario * 10 + resto;
-			numero = quociente;
-		}
+            aoContrario = aoContrario * 10 + resto;
+            numero = quociente;
+        }
 
-		return nInt == aoContrario;
-	}
+        return nInt == aoContrario;
+    }
 
-	public int SomaDosDigitos()
-	{
-		var soma = new Somatoria();
-		int n = nInt;
-		while (n > 0)
-		{
-			int quo = n / 10;
-			int digito = n - 10 * quo;
+    public int SomaDosDigitos()
+    {
+        var soma = new Somatoria();
+        int n = nInt;
+        while (n > 0)
+        {
+            int quo = n / 10;
+            int digito = n - 10 * quo;
 
-			soma.Somar(digito);
+            soma.Somar(digito);
 
-			n = quo;
-		}
-		return (int)soma.Valor;
-	}
+            n = quo;
+        }
+        return (int)soma.Valor;
+    }
 
-	public double Elevado(double a)
-	{
-		var pot = new Produtorio();
-		var vez = new Contador(1, nInt, 1);
+    public double Elevado(double a)
+    {
+        var pot = new Produtorio();
+        var vez = new Contador(1, nInt, 1);
 
-		while (vez.Prosseguir())
-		{
-			pot.Multiplicar(a);
-		}
-		return pot.Valor;
-	}
+        while (vez.Prosseguir())
+        {
+            pot.Multiplicar(a);
+        }
+        return pot.Valor;
+    }
 
-	public List<double> Fibonacci() // Função que retorna uma lista de valores reais
-	{
-		List<double> Fibonacci = new List<double>();
-		double a = 0;
-		double b = 1;
-		for (int i = 0; i < nInt; i++)
-		{
-			double temp = a;
-			a = b;
-			b = temp + b;
-			Fibonacci.Add(a);
-		}
-		return Fibonacci;
-	}
+    public List<double> Fibonacci() // Função que retorna uma lista de valores reais
+    {
+        List<double> Fibonacci = new List<double>();
+        double a = 0;
+        double b = 1;
+        for (int i = 0; i < nInt; i++)
+        {
+            double temp = a;
+            a = b;
+            b = temp + b;
+            Fibonacci.Add(a);
+        }
+        return Fibonacci;
+    }
 
+    public double AproximacaoRaizCubica(double erro) // Função que retorna a aproximação da raíz cúbica de um valor inteiro
+    {
+        double a, b;
+        a = 1;
+        do
+        {
+            b = a; // A variável "b" recebendo um palpite("a") 
+            a = (nInt / (b * b) + 2 * b) / 3; // Calculando outro palpite e colocando-a em "a"
 
+        } while (Math.Abs(a - b) > erro);
+        return a;
+    }
 
 }
--- TPProjetoI/Program.cs
+++ TPProjetoI/Program.cs
@@ -5,90 +5,112 @@ using static Utilitarios;
 
 namespace TPProjetoI
 {
-	class Program
-	{
-		private static void SeletorDeOpcoes()
-		{
-			int opcao;
+    class Program
+    {
+        private static void SeletorDeOpcoes()
+        {
+            int opcao;
 
-			do
-			{
-				Clear();
-				WritePos(10, 0, "TP - Projeto I");
+            do
+            {
+                Clear();
+                WritePos(10, 0, "TP - Projeto I");
 
-				WritePos(2, 2, "1 – Estatística de uma lista de valores lidos de um arquivo texto");
-				WritePos(2, 3, "2 – MMC entre dois valores digitados");
-				WritePos(2, 4, "3 – Raiz Cúbica de um valor digitado");
-				WritePos(2, 5, "4 – MDC por subtrações sucessivas");
-				WritePos(2, 6, "5 – Lista de números de Fibonacci");
-				WritePos(2, 7, "6 – Seno Hiperbólico");
+                WritePos(2, 2, "1 – Estatística de uma lista de valores lidos de um arquivo texto");
+                WritePos(2, 3, "2 – MMC entre dois valores digitados");
+                WritePos(2, 4, "3 – Raiz Cúbica de um valor digitado");
+                WritePos(2, 5, "4 – MDC por subtrações sucessivas");
+                WritePos(2, 6, "5 – Lista de números de Fibonacci");
+                WritePos(2, 7, "6 – Seno Hiperbólico");
 
-				WritePos(2, 20, "99 – Terminar o programa");
+                WritePos(2, 20, "99 – Terminar o programa");
 
-				WritePos(2, 22, "Opção: ");
-				opcao = int.Parse(ReadLine());
+                WritePos(2, 22, "Opção: ");
+                opcao = int.Parse(ReadLine());
 
-				switch (opcao)
-				{
-					case 1: LerArquivo(); break;
-					case 2: CalcularMMC(); break;
-					case 3: break;
-					case 4: break;
-					case 5: ListarFibonacci(); break;
-					case 6: break;
-				}
-			}
-			while (opcao != 99);
-		}
+                switch (opcao)
+                {
+                    case 1: LerArquivo(); break;
+                    case 2: CalcularMMC(); break;
+                    case 3: AproximacaoDaRaizCubica(); break;
+                    case 4: break;
+                    case 5: ListarFibonacci(); break;
+                    case 6: break;
+                }
+            }
+            while (opcao != 99);
+        }
 
-		private static void LerArquivo()
-		{
-			Clear();
-			var reader = new StreamReader(@"..\..\..\file.txt");
-			while (!reader.EndOfStream)
-			{
-				string linhaLida = reader.ReadLine();
-				double v = double.Parse(linhaLida.Substring(0, 8));
-				double p = double.Parse(linhaLida.Substring(8, 8));
+        private static void LerArquivo()
+        {
+            Clear();
+            var reader = new StreamReader(@"..\..\..\file.txt");
+            while (!reader.EndOfStream)
+            {
+                string linhaLida = reader.ReadLine();
+                double v = double.Parse(linhaLida.Substring(0, 8));
+                double p = double.Parse(linhaLida.Substring(8, 8));
 
-				var mat = new MatematicaDouble(v);
-				WriteLine($"RMQ = {mat.RaizMediaQuad(p)}");
+                var mat = new MatematicaDouble(v);
+                WriteLine($"RMQ = {mat.RaizMediaQuad(p)}");
 
-				WriteLine($"MA = {mat.Media}");
+                WriteLine($"MA = {mat.Media}");
 
-				ReadKey();
-			}
-			EsperarEnter();
-		}
+                ReadKey();
+            }
+            EsperarEnter();
+        }
 
-		private static void CalcularMMC()
-		{
-			Clear();
-			WritePos(2, 1, "Insira o primeiro valor: ");
-			int a = int.Parse(ReadLine());
-			WritePos(2, 2, "Insira o segundo valor: ");
-			int b = int.Parse(ReadLine());
-			var mat = new Matematica(a);
+        private static void CalcularMMC()
+        {
+            Clear();
+            WritePos(2, 1, "Insira o primeiro valor: ");
+            int a = int.Parse(ReadLine());
+            WritePos(2, 2, "Insira o segundo valor: ");
+            int b = int.Parse(ReadLine());
+            var mat = new Matematica(a);
 
-		}
+        }
 
-		private static void ListarFibonacci()
-		{
-			Clear();
-			WritePos(2, 2, "Insira a quantidade desejada de números de Fibonacci: ");
-			int n = int.Parse(ReadLine());
-			WriteLine();
-			var mat = new Matematica(n);
-			foreach (double a in mat.Fibonacci())
-			{
-				WriteLine(a);
-			}
-			EsperarEnter();
-		}
+        private static void AproximacaoDaRaizCubica()
+        {
+            Clear();
+            WritePos(5, 1, "Aproximação de raíz cúbica");
+            WritePos(2, 3, "Digite o a ser calculado: ");
+            int valor = int.Parse(ReadLine()); // Recebendo o valor a ser calculado
+            WritePos(2, 4, "Digite o margem de erro: ");
+            double margem = double.Parse(ReadLine());  // Recebendo a margem de erro
+            if (margem >= 0.06 || margem < 0.01) // Verificando se a margem corresponde a um valor menor que 0,06
+            {
+                WritePos(2, 6, "Valor da margem inválido.");
+                EsperarEnter();
+            }
+            else
+            {
+                var mat = new Matematica(valor); // Instanciado um objeto da classe Matematica
+                WritePos(2, 6, $"O valor aproximado da raíz cúbica de {valor} é {mat.AproximacaoRaizCubica(margem)}");
+                EsperarEnter();
+            }
 
-		static void Main(string[] args)
-		{
-			SeletorDeOpcoes();
-		}
-	}
+        }
+
+        private static void ListarFibonacci()
+        {
+            Clear();
+            WritePos(2, 2, "Insira a quantidade desejada de números de Fibonacci: ");
+            int n = int.Parse(ReadLine());
+            WriteLine();
+            var mat = new Matematica(n);
+            foreach (double a in mat.Fibonacci())
+            {
+                WriteLine(a);
+            }
+            EsperarEnter();
+        }
+
+        static void Main(string[] args)
+        {
+            SeletorDeOpcoes();
+        }
+    }
 }
